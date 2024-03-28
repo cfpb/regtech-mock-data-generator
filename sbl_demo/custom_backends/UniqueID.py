f27766b274dfd9ec6e6872a59ec70b8f8a7e121d
Matthew McBride <matthew.mcbride@cfpb.gov>
2024-03-28 13:24:58 -0400

    adding uid backend

--- sbl_demo/custom_backends/UniqueID.py
+++ sbl_demo/custom_backends/UniqueID.py
@@ -3,21 +3,40 @@ import string
 from typing import List,Iterable
 
 from mock_data.backends import AbstractBackendInterface
- 
+from mock_data.backends.Correlation import Correlation
+
 class UniqueID(AbstractBackendInterface):
     def __init__(
-            self,
+        self,
+        max_generate: int,
+        lei_first: int = 4, 
+        lei_middle: int = 14,
+        lei_last: int = 2,
+        correlation: str = Correlation.INDEPENDENT.name,
+        **distribution_kwargs,
     ) -> None:
-        def generate_lei(self):
-            n = random.randint(1, 25)
-            first = ''.join(random.choices(string.digits, k=4))
-            middle = ''.join(random.choices(string.ascii_uppercase + string.digits,k=14))
-            last = ''.join(random.choices(string.digits, k=2))
-            final = ''.join(random.choices(string.digits, k=n))
-        return ''.join(first+middle+last+final)
+        """This will create an LEI with a random number of of digits following it as per FIG guidelines. """
+        super().__init__(correlation)
+        self.lei_first = lei_first
+        self.lei_middle = lei_middle
+        self.lei_last = lei_last
+        self.max_generate = max_generate
     
-    def generate_samples(self, size: int) -> Iterable:
-        return super().generate_samples(size)
+    def generate_samples(self, size: int, directive: List = None) -> Iterable:
+        lei_ids = []
+        for i in range(size):
+            n = random.randint(1,self.max_generate)
+            first = ''.join(random.choices(string.digits, k=self.lei_first))
+            middle = ''.join(random.choices(string.ascii_uppercase + string.digits,k=self.lei_middle))
+            last = ''.join(random.choices(string.digits,k=self.lei_last))
+            final = ''.join(random.choices(string.digits, k=n))
+            lei_ids.append(''.join(first+middle+last+final))
+        return lei_ids
+    
+
+        
+
+
     
 
 
