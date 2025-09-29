f46527193fb7b8565c5c6b83c579178f2a051ddb
Alex Nongard <alexander.nongard@cfpb.gov>
2025-09-29 16:30:23 -0400

    feat: new arguments, now generates complete file

--- src/main_hmda.py
+++ src/main_hmda.py
@@ -1,13 +1,21 @@
+# This will generate a complete HMDA test file given proper config files
+
 import logging
 import os
 import argparse
 import random
+import utils
+import yaml
+import pandas as pd
 
 from mock_data import MockDataset
 
 parser = argparse.ArgumentParser('datagen')
+parser.add_argument('-f', '--yaml_file')
+parser.add_argument('-ts', '--transmittal_sheet')
+parser.add_argument('-op', '--output_filepath')
+parser.add_argument('-of', '--output_filename')
 parser.add_argument('-n', '--nrows', type=int, default=100)
-parser.add_argument('-o', '--outputfile', default="hmda.csv")
 parser.add_argument('-v', '--verbose', action='store_true')
 args = parser.parse_args()
 
@@ -19,8 +27,21 @@ os.chdir(os.path.dirname(__file__))
 
 random.seed()
 
-mock = MockDataset.read_yaml_spec("config/hmda_simplified_conventional.yaml")
+mock = MockDataset.read_yaml_spec(args.yaml_file)
 
 mock_df = mock.generate_mock_data(args.nrows)
 
-mock_df.to_csv(args.outputfile, index=False)
+# Create and append check digit for the ULI field using the utils.check_digit_gen() tool
+mock_df["uli"] = mock_df["uli"] + mock_df["uli"].apply(
+    lambda x: utils.check_digit_gen(ULI=x)
+)
+
+# Read in the transmittal sheet
+with open(args.transmittal_sheet, "r") as file:
+    ts = yaml.safe_load(file)
+ts_df = pd.DataFrame(ts)
+
+# Write the file out as a .txt following HMDA file format - TS as first row, LAR as subsequent rows
+utils.write_file(
+    path=args.output_filepath, name=args.output_filename, ts_input=ts_df, lar_input=mock_df
+)
